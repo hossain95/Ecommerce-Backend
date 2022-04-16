e8e58de90650c59a50bd04d6c43e1ca7abe6217b
hossain95 <miahossain8888@gmail.com>
2022-04-16 20:52:37 +0600

    update

--- src/main/java/com/example/ecommercebackend/repository/ShippingAddressRepository.java
+++ src/main/java/com/example/ecommercebackend/repository/ShippingAddressRepository.java
@@ -1,9 +1,11 @@
 package com.example.ecommercebackend.repository;
 
+import com.example.ecommercebackend.model.Buyer;
 import com.example.ecommercebackend.model.ShippingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
+    public ShippingAddress findByBuyer(Buyer buyer);
 }
